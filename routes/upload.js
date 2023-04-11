ad462f32cfcd0bf22aa0f81e3a7f2018562e7342
Vishnudev K <vishnudev.k@ibm.com>
2023-04-11 21:39:36 +0100

    Returning internal route

--- routes/upload.js
+++ routes/upload.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const request = require('request');
-  request('http://www.google.com', function (error, response, body) {
+  request('https://10.217.4.124:8080', function (error, response, body) {
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
