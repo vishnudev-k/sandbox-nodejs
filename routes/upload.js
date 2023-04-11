8cbe4bd41157a18a890a5bb2c7d2ea43fdf48164
Vishnudev K <vishnudev.k@ibm.com>
2023-04-11 21:36:16 +0100

    Returning google

--- routes/upload.js
+++ routes/upload.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  const request = require('request');
+  request('http://www.google.com', function (error, response, body) {
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.send(body);
+  });
+  //res.send('respond with a resource');
 });
 
 
