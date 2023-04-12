d48daebf8fcd1e82b001d39dde08f4f8f1cc5343
Vishnudev K <vishnudev.k@ibm.com>
2023-04-12 11:11:01 +0100

    Added env variable

--- routes/upload.js
+++ routes/upload.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
+
+
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   const request = require('request');
-  request('http://app-cli.image-uploader.svc.cluster.local:8080', function (error, response, body) {//https://10.217.4.124:8080
+  request(process.env.service-host-env, function (error, response, body) {//'http://app-cli.image-uploader.svc.cluster.local:8080'
     console.error('error:', error); // Print the error if one occurred
     console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
     console.log('body:', body); // Print the HTML for the Google homepage.
