828ab9a12586161108c2ffe19629ae17edd0f203
Vishnudev K <vishnudev.k@ibm.com>
2023-04-12 11:19:10 +0100

    Debug for host

--- routes/upload.js
+++ routes/upload.js
@@ -18,6 +18,22 @@ router.get('/', function(req, res, next) {
 });
 
 
+router.get('/env', function(req, res, next) {
+    res.send(process.env.service-host-env);
+});
+
+
+router.get('/host', function(req, res, next) {
+  const request = require('request');
+  request('http://app-cli.image-uploader.svc.cluster.local:8080', function (error, response, body) {//
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the HTML for the Google homepage.
+    res.send(body);
+  });
+  //res.send('respond with a resource');
+});
+
 router.post('/register', async (req, res) => {
   let result = await registerServices.register(req.body)
   if(result.code != 200) return res.status(result.code).json({
